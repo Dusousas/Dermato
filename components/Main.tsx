540e95987b7ba3d533795a0d850e74327d7fadb2
Dusousas <dusousadeveloper@gmail.com>
2025-05-26 19:39:06 -0300

    versão mobile

--- components/Main.tsx
+++ components/Main.tsx
@@ -8,21 +8,21 @@ export default function Main() {
                 <div className='maxW flex flex-col justify-center h-full'>
                     <article className='flex flex-col gap-4 lg:w-[45%]'>
                         <p className="text-BrowP font-medium tracking-widest uppercase">Skin - LoremIpsum</p>
-                        <h1 className='text-black font-semibold text-7xl'>Lorem ipsum dolor sit.</h1>
+                        <h1 className='text-black font-semibold text-4xl lg:text-7xl'>Lorem ipsum dolor sit.</h1>
                         <p className='text-BrowP'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo cum quisquam ipsa aspernatur sapiente at blanditiis suscipit unde in dolore.</p>
                     </article>
 
                     <article className='mt-10 flex flex-col items-start'>
-                        <div className='bg-BrowP text-white px-10 py-4 text-lg font-medium tracking-wide uppercase'>
+                        <div className='bg-BrowP text-white px-10 font-medium py-1 tracking-wide uppercase lg:py-4 lg:text-lg'>
                             01 Tratamentos com Laser
                         </div>
-                        <div className='bg-[#824847] text-white px-10 py-4 text-lg font-medium tracking-wide uppercase'>
+                        <div className='bg-[#824847] text-white px-10 font-medium py-1 tracking-wide uppercase lg:py-4 lg:text-lg'>
                             02 Harmonização Facial
                         </div>
-                        <div className='bg-[#9c6463] text-white px-10 py-4 text-lg font-medium tracking-wide uppercase '>
+                        <div className='bg-[#9c6463] text-white px-10 font-medium py-1 tracking-wide uppercase lg:py-4 lg:text-lg'>
                             03 Preenchimentos
                         </div>
-                        <div className='bg-[#b2807f] text-white px-10 py-4 text-lg font-medium tracking-wide uppercase w-56'>
+                        <div className='bg-[#b2807f] text-white px-10 font-medium py-1 tracking-wide uppercase lg:py-4 lg:text-lg'>
                             04 Botox
                         </div>
                     </article>
